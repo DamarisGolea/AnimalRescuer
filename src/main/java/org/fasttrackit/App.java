3462ca154cea73cf36eb2f60b14deb9df552855b
Your Name <damaris.golea@gmail.com>
2019-06-10 15:43:03 +0300

    set-up basic project structure

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,40 @@
 package org.fasttrackit;
 
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Animal animal = new Animal();
+        animal.name = "Bubico";
+        animal.age = 1;
+        animal.favoriteFood = "bones";
+        animal.levelOfHealth = 7;
+        animal.levelOfHunger = 10;
+        animal.levelOfMood = 6;
+        animal.favoriteRecreationalActivity = "running";
+
+       Adopter adopter = new Adopter();
+       adopter.name = "Caroline";
+       adopter.amountOfMoneyAvailable = 300;
+
+       Food food = new Food();
+       food.name = "Pedigree";
+       food.price = 30.99;
+       food.quantity = 100;
+       food.availabilityInStock = true;
+       food.expirationDate = LocalDate.of(2020,2,22);
+
+       RecreationalActivity recreationalActivity = new RecreationalActivity();
+       recreationalActivity.name = "running in the park";
+
+       Vet vet = new Vet();
+       vet.name = "Dr. Jason Lee ";
+       vet.specialty = "internal medicine";
+
     }
 }
