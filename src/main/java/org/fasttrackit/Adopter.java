840fec4cf4030513d9cf391b33d46f8b3dc70298
Your Name <damaris.golea@gmail.com>
2019-06-28 16:26:57 +0300

    Methods

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -7,6 +7,14 @@ public class Adopter extends Person {
         super(name, sex);
     }
 
+    public void feeding(Animal animal, Food food) {
+        System.out.println(getName() + " just gave some " + food.getName() + " food to " + animal.getName() + ".");
+    }
+
+    public void activityDoing(Animal animal, RecreationalActivity recreationalActivity) {
+        System.out.println(getName() + " and " + animal.getName() + " are " + recreationalActivity.getName() + ".");
+    }
+
     public double getAmountOfMoneyAvailable() {
         return amountOfMoneyAvailable;
     }
--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,11 +4,9 @@ import java.time.LocalDate;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         Animal dog = new Mammal("Bubico", 10);
         dog.setAge(1);
         dog.setFavoriteFood("bones");
@@ -23,13 +21,13 @@ public class App
 
         System.out.println(adopter.getAmountOfMoneyAvailable());
 
-        Food food = new Food("Pedigree", LocalDate.of(2020,2,22));
+        Food food = new Food("Pedigree", LocalDate.of(2020, 2, 22));
         food.setPrice(30.99);
         food.setQuantity(100);
         food.setAvailabilityInStock(true);
 
         System.out.println(food.getExpirationDate());
-        
+
         RecreationalActivity recreationalActivity = new RecreationalActivity("running in the park");
         recreationalActivity.setType("sport");
         recreationalActivity.setDurationInMinutes(30);
@@ -40,12 +38,19 @@ public class App
         vet.setSpecialty("internal medicine");
 
         System.out.println(vet.getName());
-        
-        PetAccessory accessory = new PetAccessory("toys",10.12);
+
+        PetAccessory accessory = new PetAccessory("toys", 10.12);
         accessory.setName("ball");
         accessory.setBrand("KONG");
         accessory.setColor("blue");
 
         System.out.println(accessory.getType());
+
+        adopter.feeding(dog, food);
+        System.out.println("Bubico ate some Pedigree.");
+
+        adopter.activityDoing(dog, recreationalActivity);
+        System.out.println("Bubico had some recreational activity.");
     }
+
 }
